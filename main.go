15f1b223af3ab6f68a248479dc3214cf59500db7
Arques <bhuvaneshkpb@gmail.com>
2026-08-15 23:53:46 +0530

    Task1 Completed: Created Http Server

--- main.go
+++ main.go
@@ -1,12 +1,68 @@
 package main
 
+import (
+	"crypto/rand"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	WSPort = ":3245"
+)
+
+type Server struct {
+	clients []*Client
+	mu      *sync.RWMutex
+}
+
+type Client struct {
+	Id   string
+	mu   *sync.RWMutex
+	conn *websocket.Conn
+}
+
+func NewClient(conn *websocket.Conn) *Client {
+	Id := rand.Text()[:9]
+	return &Client{
+		Id:   Id,
+		conn: conn,
+		mu:   new(sync.RWMutex),
+	}
+}
+
+func HandleWS(w http.ResponseWriter, r *http.Request) {
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  512,
+		WriteBufferSize: 512,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Printf("Error on HTTP conn upgrade %v\n", err.Error())
+		return
+	}
+
+}
+
 // TODO's
-// [] Create a HTTP server
+// [x] Create a HTTP server
 // [] Upgrade it to Web Socket server
 // [] Add newly connected Web Socker to the server
 // [] Add Web Socket Client
 // [] Remove the connection when the client disconnects the server
 // [] send broadcast message -> with no race condition happeing
 func main() {
+	http.HandleFunc("/", HandleWS)
 
+	err := http.ListenAndServe(WSPort, nil)
+	if err != nil {
+		log.Fatalf("Error occured in http server: %v", err.Error())
+	}
 }
