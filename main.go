417b2d6da8791b2fa1d8085bd07025b69882897e
Arques <bhuvaneshkpb@gmail.com>
2026-08-15 18:02:14 +0530

    Task defined for main.go

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+// TODO's
+// [] Create a HTTP server
+// [] Upgrade it to Web Socket server
+// [] Add newly connected Web Socker to the server
+// [] Add Web Socket Client
+// [] Remove the connection when the client disconnects the server
+// [] send broadcast message -> with no race condition happeing
 func main() {
-	
+
 }
