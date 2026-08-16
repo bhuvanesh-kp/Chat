21e8aaa5469e7d81cfb21e324e370e1eda212217
Arques <bhuvaneshkpb@gmail.com>
2026-08-16 17:56:13 +0530

    Task4 & Task5 Completed

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/rand"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,11 +15,38 @@ var (
 	WSPort = ":3245"
 )
 
+type MsgType string
+
+const (
+	MsgType_BroadCast MsgType = "broadcast"
+)
+
+type ReqType struct {
+	MsgType MsgType
+	Client  *Client
+	Data    string
+}
+
+type RespType struct {
+	MsgType  MsgType
+	SenderId string
+	Data     string
+}
+
+func NewRespType(msg *ReqType) *RespType {
+	return &RespType{
+		MsgType:  msg.MsgType,
+		SenderId: msg.Client.Id,
+		Data:     msg.Data,
+	}
+}
+
 type Server struct {
 	clients         map[string]*Client
 	mu              *sync.RWMutex
 	joinServerChan  chan *Client
 	leaveServerChan chan *Client
+	broadcastChan   chan *ReqType
 }
 
 type Client struct {
@@ -42,6 +70,7 @@ func NewServer() *Server {
 		mu:              new(sync.RWMutex),
 		joinServerChan:  make(chan *Client, 64),
 		leaveServerChan: make(chan *Client, 64),
+		broadcastChan:   make(chan *ReqType, 64),
 	}
 }
 
@@ -67,6 +96,31 @@ func (srv *Server) handleWS(w http.ResponseWriter, r *http.Request) {
 	// Thus by go's philosify "Dont communicate by sharing memory, share memory by communication" we are using channels to achive the same
 	// srv.clients[client.Id] = client
 	srv.joinServerChan <- client
+
+	go client.readMsgLoop(srv)
+}
+
+func (cl *Client) readMsgLoop(srv *Server) {
+	defer func() {
+		cl.conn.Close()
+		srv.leaveServerChan <- cl
+	}()
+
+	for {
+		_, b, err := cl.conn.ReadMessage()
+		if err != nil {
+			return
+		}
+
+		msg := new(ReqType)
+		err = json.Unmarshal(b, msg)
+		if err != nil {
+			fmt.Println("Error in unmarshalling data : ", err.Error())
+			continue
+		}
+		msg.Client = cl
+		srv.broadcastChan <- msg
+	}
 }
 
 func (srv *Server) AcceptLoop() {
@@ -76,6 +130,8 @@ func (srv *Server) AcceptLoop() {
 			srv.joinServer(c)
 		case c := <-srv.leaveServerChan:
 			srv.leaveServer(c)
+		case msg := <-srv.broadcastChan:
+			go srv.broadcastMsg(msg)
 		}
 	}
 }
@@ -88,6 +144,29 @@ func (srv *Server) joinServer(client *Client) {
 func (srv *Server) leaveServer(client *Client) {
 	delete(srv.clients, client.Id)
 	fmt.Printf("Client left the server, CId: %s\n", client.Id)
+}
+
+func (srv *Server) broadcastMsg(msg *ReqType) {
+	cl := []*Client{}
+
+	srv.mu.RLock()
+	for _, c := range srv.clients {
+		if c.Id != msg.Client.Id {
+			cl = append(cl, c)
+		}
+	}
+	srv.mu.RUnlock()
+
+	resp := NewRespType(msg)
+	for _, c := range cl {
+		err := c.conn.WriteJSON(resp)
+		if err != nil {
+			fmt.Println("Error in broadcasting msg to clientID: ", c.Id)
+			continue
+		}
+	}
+
+	fmt.Println("Broadcast completed")
 }
 
 func CreateServer() {
@@ -107,9 +186,9 @@ func CreateServer() {
 // [x] Create a HTTP server
 // [x] Upgrade it to Web Socket server
 // [x] Add newly connected Web Socker to the server
-// [] Add Web Socket Client
-// [] Remove the connection when the client disconnects the server
-// [] send broadcast message -> with no race condition happeing
+// [x] Add Web Socket Client
+// [x] Remove the connection when the client disconnects the server
+// [x] send broadcast message -> with no race condition happeing
 func main() {
 	CreateServer()
 }
--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -11,12 +12,14 @@ import (
 )
 
 type TestConfig struct {
-	ClientCount int
-	wg          *sync.WaitGroup
+	ClientCount    int
+	wg             *sync.WaitGroup
+	broadCstMsgCnt *atomic.Int64
+	targetMsgCnt   int
 }
 
-func DialerServer(wg *sync.WaitGroup) {
-
+func DialerServer(tc *TestConfig) *websocket.Conn {
+	exitChan := make(chan struct{})
 	dialer := websocket.DefaultDialer
 
 	conn, _, err := dialer.Dial(fmt.Sprintf("ws://localhost%s", WSPort), nil)
@@ -24,30 +27,77 @@ func DialerServer(wg *sync.WaitGroup) {
 		log.Fatal(err.Error())
 	}
 
-	defer func() {
-		conn.Close()
-		wg.Done()
+	go func() {
+		for {
+			time.Sleep(2 * time.Second)
+			if tc.targetMsgCnt == int(tc.broadCstMsgCnt.Load()) {
+				close(exitChan)
+				return
+			}
+		}
 	}()
 
-	fmt.Println("Connected to the server ", conn.LocalAddr().String())
-	time.Sleep(1 * time.Second)
+	go func() {
+		<-exitChan
+		conn.Close()
+		tc.wg.Done()
+	}()
+
+	// time.Sleep(2 * time.Second)
+
+	go func() {
+		for {
+			_, b, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			if len(b) > 0 {
+				tc.broadCstMsgCnt.Add(1)
+			}
+		}
+	}()
+
+	return conn
 }
 
 func TestConnection(t *testing.T) {
 	go CreateServer()
 	time.Sleep(1 * time.Second)
+	clientCount := 5
+	brCnt := 3
 
 	tc := TestConfig{
-		ClientCount: 5,
-		wg:          new(sync.WaitGroup),
+		ClientCount:    clientCount,
+		wg:             new(sync.WaitGroup),
+		broadCstMsgCnt: new(atomic.Int64),
+		targetMsgCnt:   clientCount * brCnt,
 	}
 
-	tc.wg.Add(tc.ClientCount)
+	tc.wg.Add(tc.ClientCount + 1)
+
+	brClient := DialerServer(&tc)
 
 	for range tc.ClientCount {
-		go DialerServer(tc.wg)
+		go DialerServer(&tc)
+	}
+
+	for range brCnt {
+		msg := ReqType{
+			MsgType: MsgType_BroadCast,
+			Data:    "hello from test",
+		}
+
+		time.Sleep(100 * time.Millisecond)
+		err := brClient.WriteJSON(&msg)
+		if err != nil {
+			fmt.Printf("Error Sending msg %v\n", err.Error())
+			return
+		}
+
 	}
 
 	tc.wg.Wait()
+	time.Sleep(1 * time.Second)
 	fmt.Println("exiting test case")
 }
